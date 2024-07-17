438cdb6d708821722562a42145500ace8a3e9af2
ariix00 <armandoferra2685@gmail.com>
2024-07-17 20:32:14 -0300

    05_actualizado

--- 05_funciones y parametros.dart
+++ 05_funciones y parametros.dart
@@ -11,4 +11,9 @@ int addTwoNumbersOptional(int a, [int b = 0]) {
 // b ??= 0;
 return a + b;
 
+
+}
+
+String greetPerson({ required String name, String message = 'Hola,' }){
+  return '$message Fernando';
 }
