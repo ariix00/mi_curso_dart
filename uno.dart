864b5f56d5d6e1d4362270bae4950d6bb8bad1c1
ariix00 <armandoferra2685@gmail.com>
2024-07-17 17:39:39 -0300

    primer commit

---
+++ uno.dart
@@ -0,0 +1,12 @@
+void main(){
+  //var myName = "Fernando";
+  //late final myName;
+  //String myName = "Fernando";
+  const myName = "Fernando";
+
+  print("hola $myName");
+  print("hola ${myName.toUpperCase() }");
+  print("hola ${1+1}");
+
+
+}
