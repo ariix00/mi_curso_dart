db47c2dd154cdc368d9eb4803c29abb18eed376b
ariix00 <armandoferra2685@gmail.com>
2024-07-22 20:46:19 -0300

    abstract_actualizado

--- 09_abstract_class_and_enum.dart
+++ 09_abstract_class_and_enum.dart
@@ -1,6 +1,18 @@
 void main() {
   // final windPlant = EnergyPlant();
+
+final windPlant = WindPlant(initalEnergy: 100);
+
+print('wind: ${ chargePhone(windPlant)}');
 }
+
+double chargePhone( EnergyPlant plant ){
+  if (plant.energyLeft < 10 ) {
+    throw Exception('Not enough energy');
+  }
+  return plant.energyLeft - 10;
+}
+
 
 enum PlantType { nuclear, wind, water}
 
@@ -8,5 +20,24 @@ abstract class EnergyPlant {
 
 double energyLeft;
 PlantType type; // nuclear, wind, water
+
+EnergyPlant({
+  required this.energyLeft,
+  required this.type
+});
+
+void consumeEnergy(double amount);
+
+}
+//extends o implements
+class WindPlant extends EnergyPlant {
+
+WindPlant({ required double initalEnergy})
+:super(energyLeft: initalEnergy, type: PlantType.wind);
+
+@override
+void consumeEnergy(double amount) {
+  energyLeft -=amount;
+}
 
 }
